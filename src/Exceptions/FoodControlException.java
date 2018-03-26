eca3ba71f34b53d0c69c5c8b34b3f9ad8a92e344
nanaachina <aachinalds@gmail.com>
2018-03-26 14:54:31 +0100

    Add files via upload

--- src/Exceptions/FoodControlException.java
+++ src/Exceptions/FoodControlException.java
@@ -1,8 +1,9 @@
+//<editor-fold defaultstate="collapsed" desc="comment">
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package Exceptions;
 
 /**
@@ -10,26 +11,27 @@ package Exceptions;
  * @author adm-achina
  */
 public class FoodControlException extends Exception {
-
+    
     public FoodControlException() {
     }
-
+    
     public FoodControlException(String message) {
         super(message);
     }
-
+    
     public FoodControlException(String message, Throwable cause) {
         super(message, cause);
     }
-
+    
     public FoodControlException(Throwable cause) {
         super(cause);
     }
-
+    
     public FoodControlException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
-
-
+    
+    
     
 }
+
